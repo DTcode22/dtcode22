094ff9f9719ef23bb1fbba28165ff69c0ea3cd1d
supdiff <unknownmadafaka69@gmail.com>
2025-06-28 19:53:47 +0200

    added theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import { ModeToggle } from '@/components/theme/ThemeToggle';
 export default function Home() {
-  return <h1>init</h1>;
+  return (
+    <>
+      <div>
+        <ModeToggle></ModeToggle>
+      </div>
+      <h1>npx shadcn-ui@latest cleanup</h1>
+    </>
+  );
 }
